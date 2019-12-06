483cfe9495418cd0ac9c1d01cdce712cd08756b9
gretzkiy <dias7707@gmail.com>
2019-12-07 01:46:20 +0300

    Новые методы для чтения из БД

--- EmissionsLibrary/Value.cs
+++ EmissionsLibrary/Value.cs
@@ -40,6 +40,35 @@ namespace EmissionsLibrary
             }
         }
 
+        // Получение новых значений за промежуток времени interval (в секундах)
+        public static List<Value> GetNew(string connectionString, int interval)
+        {
+            var endTime = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            var startTime = endTime - interval;
+
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                var sqlQuery = $"SELECT * FROM {tableName} " +
+                    $"WHERE {tableName}.timestampEnd BETWEEN {startTime} AND {endTime};";
+
+                return connection.Query<Value>(sqlQuery).ToList();
+            }
+        }
+
+        // Получение последних значений каждого параметра
+        public static List<Value> GetLatest(string connectionString)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                var sqlQuery = "SELECT Val.valueUuid, Val.timestampStart, Val.timestampEnd, Val.value, Val.parameterUuid " +
+                    $"FROM {tableName} Val " +
+                    $"RIGHT OUTER JOIN (SELECT parameterUuid, MAX(timestampEnd) as MaxEndTime FROM {tableName} GROUP BY parameterUuid) Latest " +
+                    "ON Val.parameterUuid = Latest.parameterUuid AND Val.timestampEnd = Latest.MaxEndTime;";
+
+                return connection.Query<Value>(sqlQuery).ToList();
+            }
+        }
+
         // Сохранение нового значения в базу данных
         public static void Create(string connectionString, Value value)
         {
